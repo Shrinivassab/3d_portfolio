62f4eabf6a3fbfa9ef60f3f2d4d0751a89efdc0e
shrinivassab <shrinivassab@gmail.com>
2023-08-31 10:56:55 -0400

    Completed Project - Added contact service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_n5mwddc",
+        "template_t8mtai8",
         {
           from_name: form.name,
           to_name: "JavaScript Mastery",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "shrinivassab@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "eEylyOCWcPzhdXBbG"
       )
       .then(
         () => {
